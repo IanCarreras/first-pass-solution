5287dd95d496276d25551427ed2c37296e3caadc
Ian <mus5002@aol.com>
2020-07-14 18:46:35 -0400

    single_number.py passing all tests

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,26 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # sort array 
+    # the loop will start at the third position
+    # and track the previous two elements in the array
+    # because the array is sorted all duplicate values will be adjacent
+    # if the middle element doesn't equal the element to either side
+    # it is the single number
+    arr.sort()
+    print(arr)
+    single = None
+    first = arr[0]
+    i = 2
+    while i < len(arr):
+        print(f'{arr[i-2]} {arr[i-1]} {arr[i]}')
+        if arr[i-1] != arr[i-2]  and arr[i-1] != arr[i]:
+            single = arr[i-1]
+            return single
+        elif i == len(arr)-1 and single == None:
+            return arr[i]
+        i += 1
 
-    pass
 
 
 if __name__ == '__main__':
